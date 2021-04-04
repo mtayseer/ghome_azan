3b71d0410226f159494ef584014af7a840859eb7
Mohammad Tayseer <m.tayseer82@gmail.com>
2021-04-04 14:26:02 +0200

    Many enhancements

--- ghome_azan.py
+++ ghome_azan.py
@@ -1,13 +1,7 @@
-import requests, googlehomepush, time, datetime, json, os, codecs, logging
+import requests, googlecontroller, time, datetime, json, os, sys, codecs, logging
 
 custom_alarms = {'08:00': 'Yusuf and Mariam, we have to leave now',
                  '07:30': 'Attention please, we have 30 minutes'}
-
-# We don't want to read prayer times from the web all the time, so we cache it
-# for every day
-current_dir = os.path.abspath(os.path.dirname(__file__))
-today = datetime.datetime.today()
-today_file = os.path.join(current_dir, today.strftime('%d-%m-%Y.prayer_times'))
 
 logging.basicConfig(level=logging.INFO)
 handler = logging.FileHandler(os.path.join(current_dir, today.strftime('%d-%m-%Y.log')))
@@ -15,6 +9,19 @@ handler.setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 logger.addHandler(handler)
 
+current_dir = os.path.abspath(os.path.dirname(__file__))
+config_file = os.path.join(current_dir, "config.json")
+
+if not os.path.exists(config_file):
+    logger.error('config.json does not exist')
+    sys.exit(1)
+
+config = json.load(open(config_file))
+
+# We don't want to read prayer times from the web all the time, so we cache it
+# for every day
+today = datetime.datetime.today()
+today_file = os.path.join(current_dir, today.strftime('%d-%m-%Y.prayer_times'))
 logger.info('Today file: %s', today_file)
 
 # Delete old prayer times cache files
@@ -24,15 +31,15 @@ for f in os.listdir(current_dir):
         os.remove(f)
 
 if os.path.exists(today_file):
-    logger.info('Today file exists. Reading from it.')
+    logger.info('Reading prayer times from local file cache.')
     prayer_times = json.load(codecs.open(today_file, encoding='utf-8'))
 else:
     logger.info('Reading prayer times from the server')
     url = today.strftime(
             'http://api.aladhan.com/v1/timings/'
             '%d-%m-%Y'
-            '?latitude=47.3718341'
-            '&longitude=8.5382787'
+            '?latitude=' + config['latitude']
+            '&longitude=' + config['longitude']
             '&method=2')
     prayer_times = requests.get(url).json()
     json.dump(prayer_times, codecs.open(today_file, 'wb', encoding='utf-8'))
@@ -43,15 +50,15 @@ for p in ['Sunset', 'Midnight', 'Imsak', 'Sunrise']:
 
 # If now is a prayer time, then cast azan sound
 now = datetime.datetime.now().strftime('%H:%M')
-gh = googlehomepush.GoogleHome('Kitchen speaker')
-if now in prayer_times['data']['timings'].values():
+gh = googlecontroller.GoogleAssistant(host=config['ip_address'])
+if True: #now in prayer_times['data']['timings'].values():
     logger.info('Casting azan at: %s', now)
-    gh.cc.set_volume(.35)
+    #gh.volume(35)
     gh.play(
         'https://ia800303.us.archive.org/5/items/Naseer.Al.Qtami.Azan/'
         'Naseer.Al.Qtami.Azan.mp3',
         'audio/mp3')
-    gh.cc.set_volume(.5)
+    #gh.volume(100)
 elif now in custom_alarms:
     gh.say(custom_alarms[now])
 else:
